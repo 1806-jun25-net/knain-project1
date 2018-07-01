821047bcee975407c05b3feea0615400fc1bb334
Wayne Knain <wknain@gra.midco.net>
2018-06-30 21:24:58 -0400

    added testing, worked on writing to file

--- Project1/Project1.Library/Location.cs
+++ Project1/Project1.Library/Location.cs
@@ -7,8 +7,12 @@ namespace Project1.Library
     public class Location
     {
         public static Dictionary<string, List<string>> history = new Dictionary<string, List<string>>();
+        public static Order order = new Order();
+        public static string toppings = string.Join(", ", Order.OrderToppings.ToArray());
+        public static string filePath = @":\Revature\knain-project1\Project1";
+
         public static List<string> values = new List<string>
-            { Order.OrderSize, Order.OrderCrust, Order.OrderToppings.ToString()};
+            { Order.OrderSize, Order.OrderCrust, toppings};
 
         public static void OrderHistoryAdd()
         {
@@ -17,7 +21,7 @@ namespace Project1.Library
 
         public static void OrderHistoryRecall()
         {
-
+            
         }
 
         public static void Inventory()
---
+++ Project1/Project1.Test/Tester.cs
@@ -0,0 +1,15 @@
+﻿using Project1.Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.XUnit
+{
+    public class Tester : Validation
+    {
+        public override string ReadNext()
+        {
+            return null;
+        }
+    }
+}
